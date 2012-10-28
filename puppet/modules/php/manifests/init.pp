df608fbe9877c3183cdfbae42b356534ac39b1ac
Niklas Laxström <niklas.laxstrom@gmail.com>
2012-10-28 13:11:06 +0000

    Translatewiki.net puppet configuration
    Started with nginx
    
    Change-Id: I7bcb0f0c8c339fac8365463154b3dd8e273b3877

---
+++ puppet/modules/php/manifests/init.pp
@@ -0,0 +1,14 @@
+class php {
+  package { ['php5-cli', 'php5-curl', 'php5-fpm', 'php5-gd', 'php5-intl',
+             'php5-mysql', 'php-pear' ]:
+    ensure => present,
+  }
+
+  service { 'php5-fpm':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => Package['php5-fpm'],
+  }
+}
---
+++ puppet/modules/puppet/manifests/init.pp
@@ -0,0 +1,5 @@
+class puppet {
+  package { 'puppet':
+    ensure => present,
+  }
+}
