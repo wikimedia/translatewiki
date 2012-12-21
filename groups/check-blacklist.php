9b68909742c91aae5556d73d1d5ae0b61999d041
Siebrand Mazeland <s.mazeland@xs4all.nl>
2012-12-21 23:04:47 +0100

    Update check blacklist.
    Change-Id: If706f0c37c6ed5fd98aa5b30426f9d32b6eff6ac

--- groups/check-blacklist.php
+++ groups/check-blacklist.php
@@ -58,6 +58,8 @@ array(
 		'logentry-move-move redir', // Optional GENDER parameter
 		'logentry-move-move redir-noredirect', // Optional GENDER parameter
 		'logentry-newusers-create2', // Optional GENDER parameters
+		'logentry-rights-autopromote', // Optional GENDER parameters
+		'logentry-rights-rights-legacy', // Optional GENDER parameters
 		'login-userblocked', // Optional GENDER parameter
 		'othercontribs', // Optional count parameter
 		'perfcachedts', // Optional time parameters
