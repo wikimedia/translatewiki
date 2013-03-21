9f394f7b26773502b825f939c917638fe422377d
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2013-03-21 17:25:17 +0530

    HTML skeleton for main page
    Image dance.jpg is CC BY Lowell Hendrix
    http://www.flickr.com/photos/ldhendrix/7389351416
    
    Change-Id: Ib39eeb48d346df833a6ceded2c420b7b88d307b9

--- MainPage/Resources.php
+++ MainPage/Resources.php
@@ -1,1 +1,22 @@
 <?php
+/**
+ * JavaScript and CSS resource definitions.
+ *
+ * @file
+ * @license GPL2+
+ */
+
+$resourcePaths = array(
+	'localBasePath' => __DIR__,
+	'remoteExtPath' => 'MainPage'
+);
+
+$wgResourceModules['ext.translate.mainpage'] = array(
+	'scripts' => 'resources/js/ext.translate.mainpage.js',
+	'styles' => 'resources/css/ext.translate.mainpage.css',
+	'dependencies' => array(
+		'ext.translate.grid',
+		'jquery.uls'
+	),
+	'position' =>'top',
+) + $resourcePaths;
