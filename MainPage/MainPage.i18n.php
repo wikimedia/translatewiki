fa928bf5db06bc8ae583179705e61112e81bd844
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-04-10 12:00:13 +0000

    Make the banner image configurable
    Makes it easy to change or add new images as we wish.
    
    Image changes once per day. Attribution can be specified.
    
    Change-Id: I0627d5d2ed6aad6bb3af16f84029eb6fc96a643a

--- MainPage/MainPage.i18n.php
+++ MainPage/MainPage.i18n.php
@@ -10,9 +10,12 @@ $messages['en'] = array(
 	'twnmp-s-projects' => 'Projects',
 	'twnmp-s-languages' => 'Languages',
 	'twnmp-s-messages' => 'Messages to translate',
+
+	'twnmp-bannerwho' => 'Image $1',
 );
 
 $messages['qqq'] = array(
 	'twnmp-desc' => '{{desc}}}',
 	'twnmp-mainpage' => 'Html title of the page.',
+	'twnmp-bannerwho' => 'Banner image attribution. $1 usually contains the name of the photographer and license abbreviation',
 );
