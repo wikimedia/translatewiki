8972119d0f3f48cccdb464f8f2000ebab8df8a32
Siebrand Mazeland <s.mazeland@xs4all.nl>
2013-10-07 12:24:07 +0200

    Add Northern Luri (lrc)
    Change-Id: Ieb01d8255cf884f2360190c322e4e739f0834893

--- FallbackSettings.php
+++ FallbackSettings.php
@@ -54,6 +54,7 @@ $wgTranslateLanguageFallbacks['lb'] = array( 'fr' );
 $wgTranslateLanguageFallbacks['lez'] = array( 'ru', 'az' ); # Robin 2011-09-27
 $wgTranslateLanguageFallbacks['lld'] = array( 'it', 'rm', 'fur' ); # Robin 2011-12-04
 $wgTranslateLanguageFallbacks['lmo'] = array( 'pms', 'eml', 'lij', 'vec' );
+$wgTranslateLanguageFallbacks['lrc'] = array( 'bqi', 'fa' );
 $wgTranslateLanguageFallbacks['mai'] = 'hi';
 $wgTranslateLanguageFallbacks['mdf'] = array( 'myv' );
 $wgTranslateLanguageFallbacks['mfe'] = array( 'fr' ); # Robin 2011-10-18
