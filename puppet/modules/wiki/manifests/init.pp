1376e72e464aa57031176762c36b849a98515241
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-10-26 08:32:51 +0000

    Add betawiki cronjobs to puppet
    Change-Id: I6cd2b9c77452aa95e825ef2b1df025440ed0f970

---
+++ puppet/modules/wiki/manifests/init.pp
@@ -0,0 +1,35 @@
+# = Class: wiki
+#
+# Configures various wiki stuff. Now mostly crontabs.
+#
+# == Parameters:
+#
+# $config:: Where the wiki config is stored.
+#
+# $user:: What user owns the wiki stuff.
+#
+#
+# == Sample Usage:
+#
+#   class { 'wiki':
+#     config => '/home/betawiki/config',
+#     user   => 'betawiki',
+#   }
+#
+class wiki ($config, $user) {
+  file { "/etc/cron.d/wikibackup":
+    content => template("wiki/wikibackup.erb"),
+  }
+
+  file { "/etc/cron.d/wikimaintenance":
+    content => template("wiki/wikimaintenance.erb"),
+  }
+
+  file { "/etc/cron.d/wikiservices":
+    content => template("wiki/wikiservices.erb"),
+  }
+
+  file { "/etc/cron.d/wikistats":
+    content => template("wiki/wikistats.erb"),
+  }
+}
