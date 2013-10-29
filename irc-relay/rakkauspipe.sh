7f43a3b644ed63f741fb4d602bfa844e4c86cbff
Niklas Laxström <niklas.laxstrom@gmail.com>
2013-10-29 14:45:32 +0000

    127.0.0.1 seems to work better than localhost
    Change-Id: I84dece3e3dbf7fdfc8df7442c1cab8f64404f165

--- irc-relay/rakkauspipe.sh
+++ irc-relay/rakkauspipe.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 WIKI=/www/translatewiki.net/w
-php /home/betawiki/config/irc-relay/logfilter.php ${WIKI}/logs/error_php | nc -u localhost 8966
+php /home/betawiki/config/irc-relay/logfilter.php ${WIKI}/logs/error_php | nc -u 127.0.0.1 8966
