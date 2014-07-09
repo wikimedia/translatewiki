6ac35f2d1ce083484d385db147e41918ed1ef51e
Siebrand Mazeland <siebrand@kitano.nl>
2014-07-09 14:33:38 +0200

    PHP repo no longer needed in Trusty, nor APC
    Change-Id: I0f7ec160ccb9fd052ac54cd3e5d99fa738895542

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -1,7 +1,4 @@
 class php {
-  include apt
-  apt::ppa { 'ppa:ondrej/php5-oldstable': }
-
   package { ['php5-fpm', 'php5-cli']:
     ensure => present,
   }
@@ -11,17 +8,10 @@ class php {
     'php5-gd',
     'php5-intl',
     'php5-mysql',
-    'php-apc',
     'php-pear',
     ]:
     ensure  => present,
     require => Package['php5-fpm'],
-  }
-
-  file { '/etc/php5/mods-available/apc.ini':
-    content => "extension=apc.so\napc.shm_size=384M",
-    require => Package['php-apc'],
-    notify  => Service['php5-fpm'],
   }
 
   service { 'php5-fpm':
