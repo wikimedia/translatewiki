a3281f4cfa2c3550426865bdde5935fee1e0c3bf
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-12-09 14:58:41 +0100

    Update nginx ssl config
    Change-Id: I6ebd084ec9f32992ebf51fcc24a2df2aff44aef5

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -54,4 +54,12 @@ class nginx {
     source  => 'puppet:///modules/nginx/offline',
   }
 
+  file { '/etc/ssl/private/rapidssl.pem':
+    source  => 'puppet:///modules/nginx/rapidssl.pem',
+  }
+
+  exec { 'dhparam':
+    command => '/usr/bin/openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048',
+    creates => '/etc/ssl/certs/dhparam.pem'
+  }
 }
