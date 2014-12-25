18db9bea5f0d9545e8f773b15431c400e4ce5f3a
Niklas Laxström <niklas.laxstrom@gmail.com>
2014-12-25 20:16:19 +0100

    Cleanups for previous changes
    Change-Id: I68b72f8162f649de6b24b1b8982b7a80959a6ad5

--- irc-relay/rakkauspipe.sh
+++ irc-relay/rakkauspipe.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-php /home/betawiki/config/irc-relay/logfilter.php /www/translatawiki.net/logs/error_php | nc -u 127.0.0.1 8966
+php /home/betawiki/config/irc-relay/logfilter.php /www/translatewiki.net/logs/error_php | nc -u 127.0.0.1 8966
