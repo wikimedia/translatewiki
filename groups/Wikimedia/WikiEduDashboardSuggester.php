108498e81529cc39e24f9b488d4b0ec84074df0f
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-06-14 19:43:08 +0200

    Add missing PHP file for WikiEduDashboard suggester
    Change-Id: I907af5317b1c4d67f927b203cf10968a46673d07

---
+++ groups/Wikimedia/WikiEduDashboardSuggester.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ */
+
+class WikiEduDashboardInsertablesSuggester {
+	public function getInsertables( $text ) {
+		$insertables = array();
+
+		// %{title}
+		$matches = array();
+		preg_match_all( '/\%{[^}]+}|%s/', $text, $matches, PREG_SET_ORDER );
+		$new = array_map( function( $match ) {
+			return new Insertable( $match[0], $match[0] );
+		}, $matches );
+		$insertables = array_merge( $insertables, $new );
+
+		// &nbsp;
+		$matches = array();
+		preg_match_all( '/&(?:[a-z]+|#\d+);/', $text, $matches, PREG_SET_ORDER );
+		$new = array_map( function( $match ) {
+			return new Insertable( $match[0], $match[0] );
+		}, $matches );
+		$insertables = array_merge( $insertables, $new );
+
+		return $insertables;
+	}
+}
