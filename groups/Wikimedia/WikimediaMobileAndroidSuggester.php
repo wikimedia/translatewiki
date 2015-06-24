e66148b00e3c39cfa168447df6f185dda8a11cf9
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-06-24 09:58:15 +0200

    Add checker and suggester for Wikipedia android app
    Change-Id: Ida03793665dcabbf5741f8e86522c028a03ef975

---
+++ groups/Wikimedia/WikimediaMobileAndroidSuggester.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ */
+
+class WikimediaMobileAndroidInsertablesSuggester {
+	public function getInsertables( $text ) {
+		// %s, %2$s %.2f
+		$matches = array();
+		preg_match_all( '/%(\d+\$|\.\d+)?[sduf]/U', $text, $matches, PREG_SET_ORDER );
+		$insertables = array_map( function( $match ) {
+			return new Insertable( $match[0], $match[0] );
+		}, $matches );
+
+		return $insertables;
+	}
+}
