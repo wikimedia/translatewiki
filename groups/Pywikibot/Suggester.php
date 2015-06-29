63ee9067213504dee5a8aa5247354c049869a14d
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-06-29 15:05:41 +0200

    Update pywikibot checker/insertables
    Parameter checks were already correct (see T98004)
    Added brace balance check as there are many links
    Added insertables for the message parameters
    
    Change-Id: I40f27473e076629c6ed54166d9ee85afad52c933

---
+++ groups/Pywikibot/Suggester.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * @author Niklas Laxström
+ * @license GPL-2.0+
+ */
+
+class PywikibotInsertablesSuggester {
+	public function getInsertables( $text ) {
+		// %(title)s
+		$matches = array();
+		preg_match_all( '\%\([^)]+\)[diouxXeEfFgGcrs]/U', $text, $matches, PREG_SET_ORDER );
+		$insertables = array_map( function( $match ) {
+			return new Insertable( $match[0], $match[0] );
+		}, $matches );
+
+		return $insertables;
+	}
+}
