469dca4bfdb34c1cad721eb508293332ca569b22
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-07-01 20:59:10 +0200

    Fix regex
    Change-Id: I083deca21a3c1636a72f6e828a21aa33e7a2a652

--- groups/Pywikibot/Suggester.php
+++ groups/Pywikibot/Suggester.php
@@ -9,7 +9,7 @@ class PywikibotInsertablesSuggester {
 	public function getInsertables( $text ) {
 		// %(title)s
 		$matches = array();
-		preg_match_all( '\%\([^)]+\)[diouxXeEfFgGcrs]/U', $text, $matches, PREG_SET_ORDER );
+		preg_match_all( '/\%\([^)]+\)[diouxXeEfFgGcrs]/U', $text, $matches, PREG_SET_ORDER );
 		$insertables = array_map( function( $match ) {
 			return new Insertable( $match[0], $match[0] );
 		}, $matches );
