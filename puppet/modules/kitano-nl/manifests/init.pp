fdfd2012bcd22fa49ee1501be81d19299eee3668
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-07-23 15:37:33 +0200

    Update kitano for nginx paths
    Change-Id: I8f001e52baad21a67eebb1635a94f5bfdc84aa24

--- puppet/modules/kitano-nl/manifests/init.pp
+++ puppet/modules/kitano-nl/manifests/init.pp
@@ -5,14 +5,9 @@
 class kitano-nl {
   include fcgiwrap
 
-  file { '/etc/nginx/sites-available/kitano.nl':
+  file { '/etc/nginx/sites/kitano.nl':
     source  => 'puppet:///modules/kitano-nl/kitano.nl',
     require => [Package['nginx'], Service['fcgiwrap']],
     notify  => Service['nginx'],
-  }
-
-  file { '/etc/nginx/sites-enabled/kitano.nl':
-    ensure => 'link',
-    target => '../sites-available/kitano.nl',
   }
 }
