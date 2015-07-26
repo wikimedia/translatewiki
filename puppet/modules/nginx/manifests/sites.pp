0460750f09a89ea71e1897cadc2b479e906a7ae9
Niklas Laxström <niklas.laxstrom@gmail.com>
2015-07-26 19:28:12 +0200

    nginx fixes
    * cache main page jpgs
    * compress svg images
    * add back org -> net redirect
    
    Change-Id: I0a145fa3b7be2d9ddac4f8575e639ff5be0498d2

--- puppet/modules/nginx/manifests/sites.pp
+++ puppet/modules/nginx/manifests/sites.pp
@@ -4,4 +4,8 @@ class nginx::sites {
   file { '/etc/nginx/sites/translatewiki.net':
     source  => 'puppet:///modules/nginx/translatewiki.net',
   }
+
+  file { '/etc/nginx/sites/translatewiki.org':
+    source  => 'puppet:///modules/nginx/translatewiki.org',
+  }
 }
