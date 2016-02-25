58d3f5c339cc55a8efa973c4a13300adda4e9d69
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-02-25 08:11:16 +0100

    Fix autosync: need full path
    Change-Id: Iaa915cf1835b1585558560463a65f0d91347d02e

--- bin/autosync.sh
+++ bin/autosync.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo blockly citationhunt codev freecol hivejs | xargs -n1 -P2 repo update
+echo blockly citationhunt codev freecol hivejs | \
+	xargs -n1 -P2 /home/betawiki/config/bin/repo update
 php /srv/mediawiki/targets/production/extensions/Translate/scripts/processMessageChanges.php \
 	--safe-import --group=out-blockly*,citationhunt,codev,out-freecol,hivejs* \
 	| xargs -l /home/betawiki/config/bin/udpcast
