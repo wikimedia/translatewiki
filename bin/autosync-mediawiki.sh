a2e5fe18b0ec0906b85fdd1d72be878ed05c5889
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-03-02 16:42:30 +0100

    Add autosync for Raymond
    Doing MediaWiki separately for Raymond, without automatic processing,
    but with cron that displays link on IRC.
    
    Change-Id: I9f0270203ca105d23114b74dbdd03ef30a3c2fb8

---
+++ bin/autosync-mediawiki.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+DIRSCRIPT="`dirname \"$0\"`"
+DIRSCRIPT="`( cd \"$DIRSCRIPT\" && pwd )`"
+
+NAME=mediawiki
+
+echo mediawiki mediawiki-extensions mediawiki-skins | xargs -n1 -P2 "$DIRSCRIPT/repo" update
+
+php /srv/mediawiki/targets/production/extensions/Translate/scripts/processMessageChanges.php \
+	--name $NAME --group=core,ext-*,mediawiki* --quiet
+
+if [ -f "/resources/caches/translatewiki.net/messagechanges.$NAME.cdb" ]
+then
+	"$DIRSCRIPT/udpcast" "Raymond: https://translatewiki.net/wiki/Special:ManageMessageGroups/$NAME"
+fi
