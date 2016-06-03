2717e105eda6c15d8ad22eac3c80fa3fe010ea0d
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-06-03 07:33:15 +0200

    Fix broken FUDForum suggester
    Catches VARs now, but PLURAL is still missing. And does not throw
    PHP notices anymore.
    
    Change-Id: I510263b860ff08cb8f711a1fd9bc8737438815cc

--- groups/FUDforum/Suggester.php
+++ groups/FUDforum/Suggester.php
@@ -12,7 +12,7 @@ class FudForumSuggester implements InsertablesSuggester {
 		$matches = [];
 		preg_match_all( '/{VAR: [^}]+}/', $text, $matches, PREG_SET_ORDER );
 		$new = array_map( function( $match ) {
-			return new Insertable( "{$match[1]}{$match[2]}", $match[1], $match[2] );
+			return new Insertable( "{$match[0]}", $match[0] );
 		}, $matches );
 		$insertables = array_merge( $insertables, $new );
 
