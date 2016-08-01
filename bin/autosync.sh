9517358c6a5120a7407636a157e4cdae05831f32
Niklas Laxström <niklas.laxstrom@gmail.com>
2016-08-01 09:47:19 +0200

    autosync.sh: send a message on a failure
    Also disable failing wikipedia-ios for now.
    
    Bug: T141752
    Change-Id: I5108aa061061d23dd8ac266b3ba32b4bac992f7a

--- bin/autosync.sh
+++ bin/autosync.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -o pipefail
 
 DIRSCRIPT="`dirname \"$0\"`"
 DIRSCRIPT="`( cd \"$DIRSCRIPT\" && pwd )`"
@@ -6,5 +8,5 @@ DIRSCRIPT="`( cd \"$DIRSCRIPT\" && pwd )`"
 cat "$DIRSCRIPT/EXTERNAL-PROJECTS" | xargs -n1 -P2 "$DIRSCRIPT/repo" update
 
 php /srv/mediawiki/targets/production/extensions/Translate/scripts/processMessageChanges.php \
-	--safe-import --group=* --skipgroup=ext-*,core,mediawiki* \
-	| xargs -l "$DIRSCRIPT/udpcast"
+	--safe-import --group=* --skipgroup=ext-*,core,mediawiki*,out-wikimedia-mobile-wikipedia-ios* \
+	| xargs -l "$DIRSCRIPT/udpcast" || "$DIRSCRIPT/udpcast" "Fatal error!"
