f578aa89661c61346d2b1e0c81e59222dfd95f75
Justin Du <justin.d128@gmail.com>
2016-12-09 15:58:22 -0600

    Support %s and %1$s as insertable parameters in the lib.reviews project
    Bug: T150244
    Change-Id: I82cd70bd492cc5cb881b63e807de12421ad3066b

---
+++ groups/lib.reviews/LibReviewsInsertablesSuggester.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Similar to Wikimedia/CitationHuntInsertablesSuggester.php
+ *
+ * @file
+ * @author Justin Du
+ * @license GPL-2.0+
+ */
+
+class LibReviewsInsertablesSuggester {
+	public function getInsertables( $text ) {
+		$insertables = [];
+
+		// %s, %1$s
+		$matches = [];
+		preg_match_all(
+			'/\%s|%1\$s/',
+			$text,
+			$matches,
+			PREG_SET_ORDER
+		);
+		$new = array_map( function( $match ) {
+			return new Insertable( $match[0], $match[0] );
+		}, $matches );
+		$insertables = array_merge( $insertables, $new );
+
+		return $insertables;
+	}
+}
