4bc68157982ab3d6bf8992df61e1985e983a1c9d
Justin Du <justin.d128@gmail.com>
2016-12-09 15:41:16 -0600

    Support %1$s and %s as insertable parameters in the MantisBT project
    Bug: T150250
    Change-Id: Ica2197ecb766e201e03c3014182aed562cd38d03

---
+++ groups/MantisBT/MantisBTInsertablesSuggester.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Similar to Nocc/NoccInsertablesSuggester.php
+ *
+ * @file
+ * @author Justin Du
+ * @license GPL-2.0+
+ */
+
+class MantisBTInsertablesSuggester {
+	public function getInsertables( $text ) {
+		$insertables = [];
+
+		// %1$s, %s
+		$matches = [];
+		preg_match_all(
+			'/%\d\$s|%s/',
+			$text,
+			$matches,
+			PREG_SET_ORDER
+		);
+		$new = array_map( function( $match ) {
+			return new Insertable( $match[0], $match[0] );
+		}, $matches );
+		$insertables = array_merge( $insertables, $new );
+
+		return $insertables;
+	}
+}
