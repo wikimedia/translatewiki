4adf5d34c56a67397fe4b66cfe4ed27150f4cc46
Niklas Laxström <niklas.laxstrom@gmail.com>
2017-12-17 16:48:37 +0100

    Puppetize certbot
    Bug: T181053
    Change-Id: I02965dc636f28609c670c3e5bb6efd0c70626a15

--- puppet/modules/nginx/manifests/ssl.pp
+++ puppet/modules/nginx/manifests/ssl.pp
@@ -5,10 +5,6 @@
 class nginx::ssl {
   include nginx
 
-  file { '/etc/ssl/private/rapidssl.pem':
-    source  => 'puppet:///modules/nginx/rapidssl.pem',
-  }
-
   exec { 'dhparam':
     command => '/usr/bin/openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048',
     creates => '/etc/ssl/certs/dhparam.pem'
@@ -18,11 +14,31 @@ class nginx::ssl {
     ensure => 'directory',
   }
 
-  file { '/etc/nginx/includes/ssl.conf':
-    source  => 'puppet:///modules/nginx/ssl.conf',
-  }
-
   file { '/etc/nginx/includes/ssl-certbot.conf':
     source  => 'puppet:///modules/nginx/ssl-certbot.conf',
+  }
+
+  class { '::letsencrypt':
+    email => 'root@translatewiki.net',
+  }
+
+  letsencrypt::certonly { 'translatewiki.net':
+    plugin               => 'webroot',
+    manage_cron          => true,
+    cron_success_command => 'systemctl reload nginx.service',
+    domains              => [
+      'translatewiki.net',
+      'translatewiki.org',
+      'lists.translatewiki.net',
+      'stats.translatewiki.net',
+      'dev.translatewiki.net',
+    ],
+    webroot_paths        => [
+      '/www/translatewiki.net/docroot',
+      '/www/translatewiki.net/docroot',
+      '/usr/lib/cgi-bin/mailman',
+      '/www/stats.translatewiki.net',
+      '/www/translatewiki.net/docroot',
+    ],
   }
 }
