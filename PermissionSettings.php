c352446fea1ac55019e954f91c10a4e8fde11199
Niklas Laxström <niklas.laxstrom@gmail.com>
2018-03-07 17:08:10 +0100

    Clean up group permissions
    Change-Id: I37cade05ad444c273839058a066da5ed7e5ffe3c

--- PermissionSettings.php
+++ PermissionSettings.php
@@ -1,90 +1,73 @@
 <?php
 
-$wgGroupPermissions['*'            ]['edit'] = false;
-$wgGroupPermissions['*'            ]['createaccount'] = false;
+$wgGroupPermissions[ '*'             ][ 'edit'                    ] = false;
+$wgGroupPermissions[ '*'             ][ 'createaccount'           ] = false;
 
-$wgGroupPermissions['user'         ]['skipcaptcha'] = false;
-$wgGroupPermissions['autoconfirmed']['skipcaptcha'] = true;
-$wgGroupPermissions['bot'          ]['skipcaptcha'] = true; // registered bots
-$wgGroupPermissions['sysop'        ]['skipcaptcha'] = true;
-$wgGroupPermissions['translator'   ]['skipcaptcha'] = true;
+$wgGroupPermissions[ 'user'          ][ 'move'                    ] = false;
+$wgGroupPermissions[ 'user'          ][ 'move-subpages'           ] = false;
+$wgGroupPermissions[ 'user'          ][ 'move-rootuserpages'      ] = false;
+$wgGroupPermissions[ 'user'          ][ 'movefile'                ] = false;
+$wgGroupPermissions[ 'user'          ][ 'move-categorypages'      ] = false;
+$wgGroupPermissions[ 'user'          ][ 'createclass'             ] = false;
 
-$wgGroupPermissions['user'         ]['move']                    = false;
-$wgGroupPermissions['user'         ]['move-subpages']           = false;
-$wgGroupPermissions['user'         ]['move-rootuserpages']      = false;
-$wgGroupPermissions['user'         ]['movefile']                = false;
-$wgGroupPermissions['user'         ]['move-categorypages']      = false;
-$wgGroupPermissions['autoconfirmed']['move']                    = false;
-$wgGroupPermissions['autoconfirmed']['move-subpages']           = false;
-$wgGroupPermissions['sysop'        ]['move']                    = true;
-$wgGroupPermissions['sysop'        ]['move-subpages']           = true;
-$wgGroupPermissions['sysop'        ]['move-rootuserpages']      = true;
-$wgGroupPermissions['translator'   ]['translate-messagereview'] = true;
-$wgGroupPermissions['bot'          ]['move']                    = true;
-$wgGroupPermissions['bot'          ]['move-subpages']           = true;
-$wgGroupPermissions['bot'          ]['move-rootuserpages']      = true;
+$wgGroupPermissions[ 'autoconfirmed' ][ 'sendemail'               ] = true;
 
-// nike 2009-08-03, 2011-03-11
-$wgGroupPermissions['*'            ]['createtalk']              = false;
-$wgGroupPermissions['user'         ]['createtalk']              = true;
-$wgGroupPermissions['*'            ]['createpage']              = false;
-$wgGroupPermissions['user'         ]['createpage']              = true;
+$wgGroupPermissions[ 'translator'    ][ 'editinterface'           ] = true;
+$wgGroupPermissions[ 'translator'    ][ 'translate'               ] = true;
+$wgGroupPermissions[ 'translator'    ][ 'translate-messagereview' ] = true;
+$wgGroupPermissions[ 'translator'    ][ 'skipcaptcha'             ] = true;
+$wgGroupsRemoveFromSelf[ 'translator' ] =
+	[ 'translator' ];
 
-// Siebrand 2009-09-27: preventing spam send from new accounts
-$wgGroupPermissions['user'         ]['sendemail']               = false;
-$wgGroupPermissions['autoconfirmed']['sendemail']               = true;
+$wgGroupPermissions[ 'offline'       ][ 'translate-import'        ] = true;
 
-$wgGroupPermissions['*'            ]['minoredit']               = true;
-$wgGroupPermissions['*'            ]['webchat']                 = true;
+$wgGroupPermissions[ 'bot'           ][ 'skipcaptcha'             ] = true;
+$wgGroupPermissions[ 'bot'           ][ 'move'                    ] = true;
+$wgGroupPermissions[ 'bot'           ][ 'move-subpages'           ] = true;
+$wgGroupPermissions[ 'bot'           ][ 'move-rootuserpages'      ] = true;
 
-$wgGroupPermissions['translator'   ]['editinterface']           = true;
-$wgGroupPermissions['translator'   ]['translate']               = true;
-$wgGroupPermissions['sysop'        ]['delete']                  = true;
-$wgGroupPermissions['translator'   ]['deletedhistory']          = true;
+$wgGroupPermissions[ 'sysop'         ][ 'import'                  ] = false;
+$wgGroupPermissions[ 'sysop'         ][ 'importupload'            ] = false;
+$wgGroupPermissions[ 'sysop'         ][ 'replacetext'             ] = false;
+$wgGroupPermissions[ 'sysop'         ][ 'smw-admin'               ] = false;
+$wgGroupPermissions[ 'sysop'         ][ 'delete'                  ] = true;
+$wgGroupPermissions[ 'sysop'         ][ 'suppressredirect'        ] = true;
+$wgGroupPermissions[ 'sysop'         ][ 'skipcaptcha'             ] = true;
 
-$wgGroupPermissions['sysop'        ]['import']                  = false;
-$wgGroupPermissions['sysop'        ]['importupload']            = false;
-$wgGroupPermissions['sysop'        ]['suppressredirect']        = true;
-# Temporary until core extends it, bug 9360
-$wgGroupPermissions['sysop'        ]['pagelang'] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'deletelogentry'          ] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'deleterevision'          ] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'interwiki'               ] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'nuke'                    ] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'pagetranslation'         ] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'userrights'              ] = false;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'suppressionlog'          ] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'translate-sandboxmanage' ] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'invitesignup'            ] = true;
+$wgGroupPermissions[ 'bureaucrat'    ][ 'suppressrevision'        ] = true;
+$wgRemoveGroups[ 'bureaucrat' ] = $wgAddGroups[ 'bureaucrat' ] =
+	[ 'sysop', 'translator', 'bot', 'offline' ];
 
-$wgGroupPermissions['bureaucrat'   ]['deletelogentry']          = true;
-$wgGroupPermissions['bureaucrat'   ]['deleterevision']          = true;
-$wgGroupPermissions['bureaucrat'   ]['interwiki']               = true;
-$wgGroupPermissions['bureaucrat'   ]['nuke']                    = true;
-$wgGroupPermissions['bureaucrat'   ]['pagetranslation']         = true;
-$wgGroupPermissions['bureaucrat'   ]['renameuser']              = false;
-$wgGroupPermissions['bureaucrat'   ]['suppressionlog']          = true;
-$wgGroupPermissions['bureaucrat'   ]['translate-sandboxmanage'] = true;
-$wgGroupPermissions['bureaucrat'   ]['userrights']              = false;
-$wgGroupPermissions['bureaucrat'   ]['invitesignup']            = true;
+$wgGroupPermissions[ 'transadmin'    ][ 'translate-manage'        ] = true;
+$wgGroupPermissions[ 'transadmin'    ][ 'replacetext'             ] = true;
+$wgGroupPermissions[ 'transadmin'    ][ 'translate-import'        ] = true;
+$wgGroupPermissions[ 'transadmin'    ][ 'nuke'                    ] = true;
 
-$wgGroupPermissions['staff'        ]['nuke']                    = true;
-$wgGroupPermissions['staff'        ]['renameuser']              = true;
-$wgGroupPermissions['staff'        ]['reset-passwords']         = true;
-$wgGroupPermissions['staff'        ]['suppressrevision']        = true;
-$wgGroupPermissions['staff'        ]['translate-import']        = true;
-$wgGroupPermissions['staff'        ]['translate-manage']        = true;
-$wgGroupPermissions['staff'        ]['usermerge']               = true;
-$wgGroupPermissions['staff'        ]['checkuser']               = true;
-$wgGroupPermissions['staff'        ]['checkuser-log']           = true;
-$wgGroupPermissions['staff'        ]['replacetext']             = true;
-$wgGroupPermissions['sysop'        ]['replacetext']             = false;
+$wgGroupPermissions[ 'staff'         ][ 'reset-passwords'         ] = true;
+$wgGroupPermissions[ 'staff'         ][ 'usermerge'               ] = true;
 
-$wgGroupPermissions['offline'      ]['translate-import'] = true;
+$wgAddGroups[ 'staff' ] = $wgRemoveGroups[ 'staff' ] = true;
 
-$wgGroupPermissions['transadmin'   ]['translate-manage'] = true;
-
-$wgAddGroups['staff'] = $wgRemoveGroups['staff'] = true;
-$wgRemoveGroups['bureaucrat'] = $wgAddGroups['bureaucrat'   ] = [ 'sysop', 'translator', 'bot', 'offline' ];
-$wgGroupsRemoveFromSelf['translator'] = [ 'translator' ];
-
-$wgSysopUserBans  = true;
-$wgSysopRangeBans = true;
-
-$wgNamespaceProtection[NS_FILE] = [ 'translate' ];
-$wgNamespaceProtection[NS_TEMPLATE] = [ 'translate' ];
-
-# Patrolling
-$wgGroupPermissions['translator']['autopatrol'] = true;
-$wgUseRCPatrol = true;
+// Remove preset smw and other extension groups
+$wgExtensionFunctions[] = function () use ( &$wgGroupPermissions ) {
+	$wgGroupPermissions[ 'staff' ] = array_merge(
+		$wgGroupPermissions[ 'smwadministrator' ],
+		$wgGroupPermissions[ 'smwcurator' ],
+		$wgGroupPermissions[ 'checkuser' ],
+		$wgGroupPermissions[ 'staff' ]
+	);
+	unset(
+		$wgGroupPermissions[ 'smwadministrator' ],
+		$wgGroupPermissions[ 'smwcurator' ],
+		$wgGroupPermissions[ 'checkuser' ]
+	);
+};
