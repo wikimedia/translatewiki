b44bb23e5a0a24396a8e3ce52bf8222e7b01ca33
Amir Aharoni <amir.aharoni@mail.huji.ac.il>
2018-08-11 21:42:38 +0300

    Create the Extensions Used by Wikimedia - Fundraising group
    This is for extensions related to Wikimedia Foundation Fundraising.
    
    These extensions are deleted because they are no longer used:
    * ContributionReporting
    * Payflow Pro Gateway (ext-di-pfpg)
    * WikimediaShopLink
    
    These extensions are moved from the Advanced grouo to the new group:
    * ContributionTracking
    * Donation Interface - Gateway Common (ext-di-gc)
    * FundraiserLandingPage
    * FundraisingTranslateWorkflow
    * LandingCheck
    
    Bug: T190047
    Change-Id: I2a3ca2d9370eed374a2a41bcbb9813c0791a7647

--- TranslateSettings.php
+++ TranslateSettings.php
@@ -81,6 +81,7 @@ $wgTranslateGroupFiles[] = "$GROUPS/MediaWiki/mwgithub.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/MediaWiki/MediaWiki.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/MediaWiki/WikimediaMainAgg.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/MediaWiki/WikimediaAdvancedAgg.yaml";
+$wgTranslateGroupFiles[] = "$GROUPS/MediaWiki/WikimediaFundraisingAgg.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/MediaWiki/WikimediaLegacyAgg.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/MediaWiki/WikimediaTechnicalAgg.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/MediaWiki/WikimediaUpcomingAgg.yaml";
