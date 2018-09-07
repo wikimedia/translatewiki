5799dea60bfeb7dbe0af0b99650a90e898c23164
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-09-07 07:27:40 +0200

    Add Commons Mass Descriptions
    Change-Id: Ic079b7f6dee42fad7001153ab71be5ab645d1f8d

--- TranslateSettings.php
+++ TranslateSettings.php
@@ -235,6 +235,7 @@ wfAddNamespace( 1206, 'Wikimedia' );
 $wgTranslateGroupFiles[] = "$GROUPS/wikidata/wikidata.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/CitationHunt.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/CommonsAndroid.yaml";
+$wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/CommonsMassDescription.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/CopyPatrol.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/crosswatch.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/GapFinder.yaml";
