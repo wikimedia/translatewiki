07323d679f5df4f40695f06f57a087269ec7ecb2
Niklas Laxström <niklas.laxstrom@gmail.com>
2018-09-18 13:28:10 +0200

    WikipediaLibrary: Tighten the parameter regex to avoid false positives
    Change-Id: I3ffb50c0282d7245bb9d9d3ee8473b74859207be

--- groups/Wikimedia/WikipediaLibrarySuggester.php
+++ groups/Wikimedia/WikipediaLibrarySuggester.php
@@ -10,7 +10,7 @@ class WPLInsertablesSuggester implements InsertablesSuggester {
 
 		// Variables of two kinds
 		$matches = [];
-		preg_match_all( '/%\([^)]+\)s|\{[^}]+\}/', $text, $matches, PREG_SET_ORDER );
+		preg_match_all( '/%\([^)]+\)s|\{[^} ]+\}/', $text, $matches, PREG_SET_ORDER );
 		$new = array_map( function( $match ) {
 			return new Insertable( $match[0], $match[0] );
 		}, $matches );
