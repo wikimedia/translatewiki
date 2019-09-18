7ba9a7385cdf5b22b988cd7fb3bb902fb7df85fc
Niklas Laxström <niklas.laxstrom@gmail.com>
2019-09-18 14:50:21 +0200

    Update to PHP 7.3
    Includes changes to composer command line which was not working
    after updating composer (necessary to work with PHP 7.3).
    
    Change-Id: I19488e810443ad6b90b4f61e159032bf5503776f

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -8,16 +8,16 @@ class php {
   apt::ppa { 'ppa:ondrej/php': }
 
   package { [
-    'php7.1-cli',
-    'php7.1-curl',
-    'php7.1-dba',
-    'php7.1-fpm',
-    'php7.1-gd',
-    'php7.1-intl',
-    'php7.1-mbstring',
-    'php7.1-mysql',
-    'php7.1-opcache',
-    'php7.1-xml',
+    'php7.3-cli',
+    'php7.3-curl',
+    'php7.3-dba',
+    'php7.3-fpm',
+    'php7.3-gd',
+    'php7.3-intl',
+    'php7.3-mbstring',
+    'php7.3-mysql',
+    'php7.3-opcache',
+    'php7.3-xml',
     'php-memcached',
     'php-yaml',
   ]:
@@ -25,15 +25,15 @@ class php {
     require => Apt::Ppa['ppa:ondrej/php'],
   }
 
-  file { '/etc/php/7.1/fpm/pool.d/www.conf':
+  file { '/etc/php/7.3/fpm/pool.d/www.conf':
     source  => 'puppet:///modules/php/www.conf',
-    require => Package['php7.1-fpm'],
+    require => Package['php7.3-fpm'],
     notify  => Service['php-fpm'],
   }
 
   service { 'php-fpm':
     ensure => running,
     enable => true,
-    name   => 'php7.1-fpm.service',
+    name   => 'php7.3-fpm.service',
   }
 }
