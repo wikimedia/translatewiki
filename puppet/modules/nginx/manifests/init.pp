e1251e5676e6bd00064a9e8c242bcca523f29ccb
Siebrand Mazeland <siebrand@kitano.nl>
2019-12-06 19:24:53 +0100

    Work around apt::ppa not supported on Debian
    Not the most beautiful work-around...
    
    Change-Id: Ic3263751c5241681746e16bf9b4feb0ae66bb3ce

--- puppet/modules/nginx/manifests/init.pp
+++ puppet/modules/nginx/manifests/init.pp
@@ -2,32 +2,33 @@
 #
 # Provides installation and configuration information for the nginx package.
 #
+# Manual actions:
+# apt install curl gnupg2 lsb-release
+# echo "deb http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" > /etc/apt/sources.list.d/nginx.list
+# curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 class nginx {
-  include apt
-  apt::ppa { 'ppa:nginx/stable': }
-
-  package { 'nginx':
-    ensure => present,
-    name   => 'nginx-light',
-  }
-
+  ensure_packages([
+    'nginx-light',
+  ], {
+    ensure => 'present',
+  })
   service { 'nginx':
     ensure  => running,
     enable  => true,
-    require => Package['nginx'],
+    require => Package['nginx-light'],
   }
 
   File {
-    require => Package['nginx'],
+    require => Package['nginx-light'],
     notify  => Service['nginx'],
   }
 
   file { '/etc/nginx/nginx.conf':
-    source  => 'puppet:///modules/nginx/nginx.conf',
+    source => 'puppet:///modules/nginx/nginx.conf',
   }
 
   file { '/etc/nginx/mime.types':
-    source  => 'puppet:///modules/nginx/mime.types',
+    source => 'puppet:///modules/nginx/mime.types',
   }
 
   file { '/etc/nginx/sites':
@@ -35,6 +36,6 @@ class nginx {
   }
 
   file { '/etc/logrotate.d/nginx':
-    source  => 'puppet:///modules/nginx/logrotate'
+    source => 'puppet:///modules/nginx/logrotate'
   }
 }
