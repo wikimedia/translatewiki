6b003bfeee90c6898e1c42e80a21bcaa427153f0
Siebrand Mazeland <siebrand@kitano.nl>
2019-12-06 20:42:09 +0100

    Fixes for kitano.nl
    Change-Id: Ib931df9584ef1229307de64b719ae989be3effce

--- puppet/modules/kitanonl/manifests/init.pp
+++ puppet/modules/kitanonl/manifests/init.pp
@@ -5,9 +5,27 @@
 class kitanonl {
   include fcgiwrap
 
+  $directories = [
+    '/www',
+    '/www/kitano.nl',
+  ]
+  ensure_resource('file', $directories, {
+    'ensure' => 'directory',
+    'owner'  => 'betawiki',
+    'group'  => 'users'
+  })
+
+  file { '/www/kitano.nl/index.html':
+    source  => 'puppet:///modules/kitanonl/index.html',
+    require => File['/www/kitano.nl'],
+  }
+
   file { '/etc/nginx/sites/kitano.nl':
     source  => 'puppet:///modules/kitanonl/kitano.nl',
-    require => [Package['nginx-light'], Service['fcgiwrap']],
+    require => [
+      Package['nginx-light'],
+      Service['fcgiwrap']
+    ],
     notify  => Service['nginx'],
   }
 }
