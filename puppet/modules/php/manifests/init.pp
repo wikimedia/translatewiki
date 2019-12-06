47c6c22809373d9201f812a8e4fb3b075c590268
Siebrand Mazeland <siebrand@kitano.nl>
2019-12-06 18:51:33 +0100

    Use PHP 7.3 from Buster
    Change-Id: I34881e628ba66f97c6dc61fa6e3a66afe88dea37

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -3,37 +3,32 @@
 # Configures php for MediaWiki to run in cli and fastcgi modes.
 #
 class php {
-  include ::apt
-
-  apt::ppa { 'ppa:ondrej/php': }
-
   package { [
-    'php7.3-cli',
-    'php7.3-curl',
-    'php7.3-dba',
-    'php7.3-fpm',
-    'php7.3-gd',
-    'php7.3-intl',
-    'php7.3-mbstring',
-    'php7.3-mysql',
-    'php7.3-opcache',
-    'php7.3-xml',
+    'php-cli',
+    'php-curl',
+    'php-dba',
+    'php-fpm',
+    'php-gd',
+    'php-intl',
+    'php-mbstring',
+    'php-mysql',
+    'php-opcache',
+    'php-xml',
     'php-memcached',
     'php-yaml',
   ]:
-    ensure  => present,
-    require => Apt::Ppa['ppa:ondrej/php'],
+    ensure => present,
   }
 
   file { '/etc/php/7.3/fpm/pool.d/www.conf':
     source  => 'puppet:///modules/php/www.conf',
-    require => Package['php7.3-fpm'],
+    require => Package['php-fpm'],
     notify  => Service['php-fpm'],
   }
 
   service { 'php-fpm':
     ensure => running,
     enable => true,
-    name   => 'php7.3-fpm.service',
+    name   => 'php-fpm.service',
   }
 }
