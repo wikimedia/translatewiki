942ebb13410b8767c88466ec1537ba833805959d
Siebrand Mazeland <siebrand@kitano.nl>
2019-12-20 17:50:20 +0100

    Move most configuration over to hiera and yaml
    Change-Id: I6d0a5063c2f2d1c13ee69438e87ba6ee9a9fee01

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,34 +1,6 @@
-File {
-  owner => 'root',
-  group => 'root',
-  mode  => '0644',
-}
+lookup('classes', {merge => unique}).include
 
 node 'web2.translatewiki.net' {
-  include base
-  include base::vcs
-  include sshd
-  include users
-  include hostname
-
-  include awstats
-  include php
-  include logrotate
-  include mariadb
-  include memcached
-  include nginx::sites
-
-  class { 'eximconf':
-    local_interfaces => [
-    '152.89.106.205',
-    '2a03:4000:39:55d:5400:a2ff:fe21:b3ea',
-    ]
-  }
-
-  include profile::mwelasticsearch
-
-  include kitanonl
-
   mount { '/scratch':
     ensure  => 'mounted',
     device  => '46.38.248.210:/voln264518a1',
@@ -36,23 +8,4 @@ node 'web2.translatewiki.net' {
     options => 'rw',
     require => File['/scratch'],
   }
-
-  class { 'backup':
-    databases => ['translatewiki_net'],
-  }
-
-  class { 'wiki':
-    config => '/home/betawiki/config',
-    user   => 'betawiki',
-  }
-  include wiki::irc
-
-  ::keyholder::agent { 'l10n-bot':
-    trusted_groups => ['l10n-bot'],
-    priv_key_path  => '/root/keyholder/l10n-bot',
-  }
-
-  sysctl { 'net.ipv4.tcp_window_scaling': value => '1' }
-  sysctl { 'net.ipv4.tcp_slow_start_after_idle': value => '0' }
-  sysctl { 'net.ipv4.tcp_fastopen': value => '3' }
 }
