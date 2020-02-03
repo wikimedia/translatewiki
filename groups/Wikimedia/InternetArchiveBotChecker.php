bbc00dd53c28e84d0dab9bc6f777f56d93016f3b
Abijeet <abijeetpatro@gmail.com>
2020-02-03 20:08:36 +0530

    Update InternetArchiveBot to the new validator framework
    Following validators / insertables have been added,
    
      * Add an insertable for anchor tags
      * Add an insertable validator for variables: {{userflag}}
    
    Bug: T239216
    Change-Id: If1552856cf542ae1f5567a6c95e94bbfa7b4e513

--- groups/Wikimedia/InternetArchiveBotChecker.php
+++
@@ -1,7 +0,0 @@
-<?php
-class InternetArchiveBotChecker extends MessageChecker {
-	protected function variablesCheck( $messages, $code, &$warnings ) {
-		// Variables like {{variable}}
-		return parent::parameterCheck( $messages, $code, $warnings, '/{{[a-zA-Z_]+}}/' );
-	}
-}
