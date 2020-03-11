b85f37f5b7f036e49c6db0558eb52b83867bafcc
Niklas Laxström <niklas.laxstrom@gmail.com>
2020-03-11 12:07:14 +0100

    puppet: fix keyholder
    Neither the service nor the key was being applied. Enable the
    service by default, since it does not require any keys to function.
    
    Add a small hack to site.pp that allows defining the keys in hiera
    data. Alternative would be to add a new class to keyholder or in a
    new module that does that.
    
    Change-Id: Ia28bccf4afe90e7143a52cc398737412a0a5fc75

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,1 +1,8 @@
 lookup('classes', {merge => unique}).include
+
+# It is not possible to create resources directly in hiera data
+lookup('keyholder::agent').each |$name, $cfg| {
+  ::keyholder::agent {
+    "${name}": * => $cfg,
+  }
+}
