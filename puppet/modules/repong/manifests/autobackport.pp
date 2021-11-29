ba80703fdeaf11f729a0fe5218200135099afdad
Niklas Laxström <niklas.laxstrom@gmail.com>
2021-11-29 13:50:31 +0200

    puppet: Schedule weekly backports for MediaWiki
    execute-and-notify: pass params to the script
    
    Bug: T296808
    Change-Id: I5c0af7ae312639b20a0ebf69311ec77736022eac

---
+++ puppet/modules/repong/manifests/autobackport.pp
@@ -0,0 +1,32 @@
+# = Class: repong::autobackport
+#
+# Sets up automated backports using systemd timers
+#
+class repong::autobackport (
+  String $l10nbot_user,
+  String $bin_dir,
+) {
+  systemd::unit_file { 'autobackport-mediawiki@.service':
+    content => template('repong/autobackport-mediawiki@.service.erb'),
+  }
+
+  Repong::Autobackport_mediawiki_timer {
+    require => User[ $l10nbot_user ],
+  }
+
+  # Using early European mornings when CI has low use
+  repong::autobackport_mediawiki_timer { 'REL1_35':
+    branch => 'REL1_35',
+    when   => 'Mon 06:30:00',
+  }
+
+  repong::autobackport_mediawiki_timer { 'REL1_36':
+    branch => 'REL1_36',
+    when   => 'Tue 06:30:00',
+  }
+
+  repong::autobackport_mediawiki_timer { 'REL1_37':
+    branch => 'REL1_37',
+    when   => 'Wed 06:30:00',
+  }
+}
---
+++ puppet/modules/repong/manifests/autobackport_mediawiki_timer.pp
@@ -0,0 +1,14 @@
+# = Type: repong::autobackport_mediawiki_timer
+define repong::autobackport_mediawiki_timer (
+  String $branch,
+  String $when,
+  Boolean $active = true,
+) {
+  $ensure = $active ? { true => 'present', default => 'absent' };
+  systemd::timer { "autobackport-mediawiki-${branch}.timer":
+    ensure        => $ensure,
+    timer_content => template('repong/autobackport-mediawiki.timer.erb'),
+    active        => $active,
+    enable        => $active,
+  }
+}
