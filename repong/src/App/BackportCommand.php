51e99777853d31a269aa68cfa518891a499a7e85
Niklas Laxström <niklas.laxstrom@gmail.com>
2021-12-08 09:36:12 +0200

    Increase timeout for backports
    MediaWiki core seems to fail most of the time due to the limit.
    
    Bug: T296808
    Change-Id: Ic37939c73f69ac439c0c9820e5dfe5f36a6a4f4c

--- repong/src/App/BackportCommand.php
+++ repong/src/App/BackportCommand.php
@@ -72,7 +72,7 @@ class BackportCommand extends Command {
 			$command = "$updateCommand && $backportCommand || $resetCommand";
 
 			$process = new Process( $command );
-			$process->setTimeout( 60 );
+			$process->setTimeout( 120 );
 			$processes->attach( $process );
 		}
 
