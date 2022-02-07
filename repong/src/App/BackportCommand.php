1c8aa833f108fa0c64274cbcfd66f2afafeaffec
Niklas Laxström <niklas.laxstrom@gmail.com>
2022-02-07 17:06:42 +0200

    Increase backports timeout again
    Backports for 1.35 are timing out from time to time.
    
    Follows-Up: Ic80ea58a39972dc0372d588fd1095be62d012b9c
    Change-Id: I63620cdeabb644e0ed7d86979be4cc9a5254dff8

--- repong/src/App/BackportCommand.php
+++ repong/src/App/BackportCommand.php
@@ -72,7 +72,7 @@ class BackportCommand extends Command {
 			$command = "$updateCommand && $backportCommand || $resetCommand";
 
 			$process = new Process( $command );
-			$process->setTimeout( 240 );
+			$process->setTimeout( 300 );
 			$processes->attach( $process );
 		}
 
