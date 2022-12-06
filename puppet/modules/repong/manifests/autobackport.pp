b2f865607aed908c42efae67b858b4eadb382545
Reedy <reedy@wikimedia.org>
2022-12-06 12:23:57 +0000

    autobackport.pp: Move REL1_39 backports to Tuesday
    Change-Id: I8faa97ad336781e19b329f9932b76d0819276dd0

--- puppet/modules/repong/manifests/autobackport.pp
+++ puppet/modules/repong/manifests/autobackport.pp
@@ -15,18 +15,21 @@ class repong::autobackport (
   }
 
   # Using early European mornings when CI has low use
+  # LTS; supported till September 2023
   repong::autobackport_mediawiki_timer { 'REL1_35':
     branch => 'REL1_35',
     when   => 'Mon 06:30:00',
   }
 
+  # supported till June 2023
   repong::autobackport_mediawiki_timer { 'REL1_38':
     branch => 'REL1_38',
     when   => 'Wed 06:30:00',
   }
 
+  # LTS; supported till November 2025
   repong::autobackport_mediawiki_timer { 'REL1_39':
     branch => 'REL1_39',
-    when   => 'Thu 06:30:00',
+    when   => 'Tue 06:30:00',
   }
 }
