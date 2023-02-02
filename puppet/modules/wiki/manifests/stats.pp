5960d4c8c2ede3847bc16b858fa088bd858ab82a
Niklas Laxström <niklas.laxstrom@gmail.com>
2023-02-02 16:38:57 +0200

    Fix and update stats-newcorelangs
    * Switch to using l10n-bot user, since it needs to access git repo
    * Start using run.php
    
    See https://translatewiki.net/w/i.php?diff=prev&oldid=11408483
    
    Change-Id: I8e0208aa63ccd84e4ad09fc9e9d881852f753bc3

--- puppet/modules/wiki/manifests/stats.pp
+++ puppet/modules/wiki/manifests/stats.pp
@@ -4,6 +4,7 @@
 class wiki::stats (
   String $config_dir,
   String $maintenance_user,
+  String $l10nbot_user,
 ) {
   file { '/etc/cron.d/wikistats':
     content => template('wiki/wikistats.erb'),
