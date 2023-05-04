aba19fa336f27394283f21e1ebf0fea8cc5071e8
Niklas Laxström <niklas.laxstrom@gmail.com>
2023-05-04 13:20:37 +0300

    Set Vector 2022 as the default skin
    Has better mobile support. The Special:Translate UI does not scale
    well to smaller screens, but it seems usable.
    
    Bug: T268340
    Change-Id: Ida4a8b090899ddaded86028948d8b190369455fa

--- mw-config/ExtensionSettings.php
+++ mw-config/ExtensionSettings.php
@@ -1,7 +1,7 @@
 <?php
 
 wfLoadSkins( [ 'Vector', 'MonoBook', 'Timeless' ] );
-$wgVectorDefaultSkinVersion = '1';
+$wgDefaultSkin = 'vector-2022';
 
 // Extensions which we don't specify any configuration
 wfLoadExtensions( [
