9f6b3cdd605e72a9f4b73f66bc3665ba8fafa46a
Abijeet <abijeetpatro@gmail.com>
2023-07-03 14:44:26 +0530

    Increase backports timeout to 900 seconds
    Change-Id: I2bd466d39f3824bacc7c80f8ae479cec82e62eab

--- repong/src/App/BackportCommand.php
+++ repong/src/App/BackportCommand.php
@@ -72,7 +72,7 @@ class BackportCommand extends Command {
 			$command = "$updateCommand && $backportCommand || $resetCommand";
 
 			$process = new Process( $command );
-			$process->setTimeout( 720 );
+			$process->setTimeout( 900 );
 			$processes->attach( $process );
 		}
 
