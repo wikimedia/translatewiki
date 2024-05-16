50281148c2584ed4fb2f5000840387b410dc6fb3
Niklas Laxström <niklas.laxstrom@gmail.com>
2024-05-16 14:51:22 +0300

    Convert newcorelangs from cron to systemd timer (1/2)
    Bug: T274756
    Change-Id: Iacf2a73fd5b503c90607ae8b00a96464724d6f6d

--- puppet/modules/wiki/manifests/stats.pp
+++ puppet/modules/wiki/manifests/stats.pp
@@ -3,10 +3,16 @@
 # Enables daily stat scripts
 class wiki::stats (
   String $config_dir,
-  String $maintenance_user,
   String $l10nbot_user,
 ) {
   file { '/etc/cron.d/wikistats':
-    content => template('wiki/wikistats.erb'),
+    ensure => absent,
+  }
+
+  systemd::timer { 'mw-newcorelangs.timer':
+    timer_content   => template('wiki/mw-newcorelangs.timer.erb'),
+    service_content => template('wiki/mw-newcorelangs.service.erb'),
+    active          => true,
+    enable          => true,
   }
 }
