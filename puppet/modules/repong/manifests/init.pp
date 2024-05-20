wikimedia/translatewiki

commit 5ce3ca7364afac3c15a978e4b3d3e9389718d6c3

Expand and convert repong cleanups to systemd timers
Shorten cache file age to one year.
Add repong purge to remove unused repositories.

Bug: T274756
Change-Id: Ife92a02e02fae3caaa697bfb5275cd6758e04458

puppet/modules/repong/manifests/init.pp
@@ -55,4 +55,11 @@ class repong (
     content => 'export',
     owner   => $l10nbot_user,
   }
+
+  systemd::timer { 'repong-cleanups.timer':
+    timer_content   => template('repong/repong-cleanups.timer.erb'),
+    service_content => template('repong/repong-cleanups.service.erb'),
+    active          => true,
+    enable          => true,
+  }
 }

bin/repong-cleanups
@@ -1,0 +1,14 @@
+#!/bin/bash
+set -o nounset -o pipefail -o errexit
+
+. /etc/wikisettings
+find /resources/caches/$WIKINAME/ -maxdepth 1 -ctime +365 -print0 | xargs -0 -I{} rm -rf -- "{}"
+
+# shellcheck source=bin/synclock
+. "$DIRSCRIPT/synclock"
+
+cd "$AUTOIMPORT_DIR"
+php "$DIRSCRIPT/../repong/repong.php" purge --really
+
+cd "$AUTOEXPORT_DIR"
+php "$DIRSCRIPT/../repong/repong.php" purge --really

puppet/modules/repong/templates/repong-cleanups.service.erb
@@ -1,0 +1,8 @@
+# This file is managed by puppet
+[Unit]
+Description=Cleanups stale files related to repository handling
+
+[Service]
+ExecStart=<%= @bin_dir %>/repong-cleanups
+ProtectHome=read-only
+ProtectSystem=full

puppet/modules/repong/templates/repong-cleanups.timer.erb
@@ -1,0 +1,11 @@
+# This file is managed by puppet
+[Unit]
+Description=Run repong-cleanups on a defined schedule
+
+[Timer]
+OnCalendar=weekly
+AccuracySec=12h
+Persistent=true
+
+[Install]
+WantedBy=timers.target

puppet/modules/wiki/templates/wikimaintenance.erb
@@ -1,3 +1,2 @@
 # file managed by puppet
 PATH=<%= @config_dir %>/bin:/usr/local/bin:/bin:/usr/bin
-@monthly <%= @maintenance_user %> find /resources/caches/<%= @domain %>/ -maxdepth 1 -ctime +1000 -print0 | xargs -0 -I{} rm -rf -- "{}"

commit 9bfc7b37241cde7ec3be7aaaa0c1f0b0a979072b

Fix repong-cleanups service
The executable in the service was not found because the path was
incorrect because $bin_dir was not declared in the scope.

Fixes: 5ce3ca7364afac3c15a978e4b3d3e9389718d6c3
Change-Id: Ic6cd4f6dcb97d46799d289fa463638fe1b0aa876

puppet/modules/repong/manifests/init.pp
@@ -7,6 +7,7 @@ class repong (
   String $l10nbot_user,
   String $import_dir,
   String $export_dir,
+  String $bin_dir,
 ) {
   Exec {
     require => User[ $l10nbot_user ],