wikimedia/translatewiki

commit 791036174ab9c3baafbcf79cb69c54cc6f800d8a

repong: Update symfony/* to latest version supporting PHP 7.4
Bug: T248085
Change-Id: Icfa8fa96e53080409acfc4a779c012bc4ac49734

repong/src/App/BackportCommand.php
@@ -22,7 +22,7 @@ class BackportCommand extends Command {
 		$this->addOption( 'filter', null, InputOption::VALUE_REQUIRED );
 	}
 
-	protected function execute( InputInterface $input, OutputInterface $output ) {
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		$this->parallelism = min( self::MAX_CONNECTIONS, $this->parallelism );
 		$project = $input->getArgument( 'project' );
 		$variant = $input->getOption( 'variant' ) ?: $this->defaultVariant;
@@ -71,9 +71,11 @@ class BackportCommand extends Command {
 
 			$command = "$updateCommand && $backportCommand || $resetCommand";
 
-			$process = new Process( $command );
+			$process = Process::fromShellCommandline( $command );
 			$process->setTimeout( 900 );
 			$processes->attach( $process );
+
+			return 0;
 		}
 
 		$this->runParallelWithOutput( $processes, $output );

repong/src/App/Command.php
@@ -84,7 +84,7 @@ abstract class Command extends SymfonyCommand {
 		// Step 1: Handle repo generator
 		if ( isset( $config[ 'repos' ][ '@generator' ] ) ) {
 			$generator = $config[ 'repos' ][ '@generator' ];
-			$process = new Process( $generator );
+			$process = Process::fromShellCommandline( $generator );
 			$process->setWorkingDirectory( $this->bindir );
 			$process->setTimeout( 15 );
 			$process->mustRun();

repong/src/App/CommitCommand.php
@@ -36,7 +36,7 @@ class CommitCommand extends Command {
 		);
 	}
 
-	protected function execute( InputInterface $input, OutputInterface $output ) {
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		$this->parallelism = min( self::MAX_CONNECTIONS, $this->parallelism );
 		$project = $input->getArgument( 'project' );
 		$variant = $input->getOption( 'variant' ) ?: $this->defaultVariant;
@@ -110,7 +110,7 @@ class CommitCommand extends Command {
 				throw new RuntimeException( "Unknown repo type '$type' for repository: $name" );
 			}
 
-			$process = new Process( $command );
+			$process = Process::fromShellCommandline( $command );
 			$process->setTimeout( 300 );
 			$process->setWorkingDirectory( $base );
 			$processes->attach( $process );
@@ -123,12 +123,14 @@ class CommitCommand extends Command {
 		if ( $mergePattern ) {
 			$command = $this->bindir . "/merge-wmgerrit-patches '$mergePattern'";
 			echo $command . "\n";
-			$mergeProcess = new Process( $command );
+			$mergeProcess = Process::fromShellCommandline( $command );
 			$mergeProcess->setTimeout( 600 );
 			$mergeProcess->mustRun();
 		}
 
 		$this->makePullRequests( $config, $base, $output );
+
+		return 0;
 	}
 
 	private function makePullRequests( array $config, string $base, OutputInterface $output ): void {
@@ -238,7 +240,7 @@ class CommitCommand extends Command {
 
 	/** Check whether there are changes for a pull request (compared to target branch). */
 	private function hasChanges( string $repositoryPath, string $branch ): bool {
-		$process = new Process( "git log origin/$branch..HEAD" );
+		$process = Process::fromShellCommandline( "git log origin/$branch..HEAD" );
 		$process->setWorkingDirectory( $repositoryPath );
 		$process->setTimeout( 5 );
 		$process->run();

repong/src/App/DiffCommand.php
@@ -20,7 +20,7 @@ class DiffCommand extends Command {
 		$this->addOption( 'filter', null, InputOption::VALUE_REQUIRED );
 	}
 
-	protected function execute( InputInterface $input, OutputInterface $output ) {
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		$project = $input->getArgument( 'project' );
 		$variant = $input->getOption( 'variant' ) ?: $this->defaultVariant;
 		$filter = $input->getOption( 'filter' );
@@ -48,12 +48,14 @@ class DiffCommand extends Command {
 				throw new RuntimeException( "Unknown repo type '$type' for repository: $name" );
 			}
 
-			$process = new Process( $command );
+			$process = Process::fromShellCommandline( $command );
 			$process->setTimeout( 10 );
 			$process->setWorkingDirectory( $base );
 			$processes->attach( $process );
 		}
 
 		$this->runParallelWithOutput( $processes, $output );
+
+		return 0;
 	}
 }

repong/src/App/ExportCommand.php
@@ -19,7 +19,7 @@ class ExportCommand extends Command {
 		$this->addOption( 'filter', null, InputOption::VALUE_REQUIRED );
 	}
 
-	protected function execute( InputInterface $input, OutputInterface $output ) {
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		$project = $input->getArgument( 'project' );
 		$variant = $input->getOption( 'variant' ) ?: $this->defaultVariant;
 		$filter = $input->getOption( 'filter' );
@@ -49,7 +49,7 @@ class ExportCommand extends Command {
 
 		$groupSpec = $filter ?? $config[ 'group' ];
 		$command = "$expander '$groupSpec'";
-		$process = new Process( $command );
+		$process = Process::fromShellCommandline( $command );
 		$process->setTimeout( 10 );
 		$process->mustRun();
 		$groupsOutput = trim( $process->getOutput() );
@@ -62,7 +62,7 @@ class ExportCommand extends Command {
 				$formatter->escape( $config[ 'group' ] )
 			);
 			$output->write( $msg );
-			return;
+			return 0;
 		}
 
 		$groups = explode( "\n", $groupsOutput );
@@ -73,11 +73,13 @@ class ExportCommand extends Command {
 			$jobOptions = $defaultOptions;
 			$jobOptions[ 'group' ] = $group;
 			$command = $this->buildCommandline( $exporter, $jobOptions );
-			$process1 = new Process( $command );
+			$process1 = Process::fromShellCommandline( $command );
 			$process1->setTimeout( 300 );
 			$processes->attach( $process1 );
 		}
 
 		$this->runParallelWithOutput( $processes, $output );
+
+		return 0;
 	}
 }

repong/src/App/ListCommand.php
@@ -12,8 +12,10 @@ class ListCommand extends Command {
 		$this->setDescription( 'Lists all projects defined in repoconfig.yaml' );
 	}
 
-	protected function execute( InputInterface $input, OutputInterface $output ) {
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		unset( $this->config[ '@meta' ] );
 		echo implode( "\n", array_keys( $this->config ) ) . "\n";
+
+		return 0;
 	}
 }

repong/src/App/PurgeCommand.php
@@ -25,7 +25,7 @@ class PurgeCommand extends Command {
 		);
 	}
 
-	protected function execute( InputInterface $input, OutputInterface $output ) {
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		$knownPaths = [];
 		foreach ( array_keys( $this->config ) as $project ) {
 			if ( $project === '@meta' ) {
@@ -48,13 +48,15 @@ class PurgeCommand extends Command {
 		foreach ( self::getUnknownDirectories( $this->getBase(), '', $knownPathsString ) as $path ) {
 			$output->writeln( $path );
 			if ( $really ) {
-				$process = new Process( "rm -rf '$path'" );
+				$process = Process::fromShellCommandline( "rm -rf '$path'" );
 				$process->setWorkingDirectory( $this->getBase() );
 				$processes->attach( $process );
 			}
 		}
 
 		$this->runParallelWithOutput( $processes, $output );
+
+		return 0;
 	}
 
 	private static function getUnknownDirectories(

repong/src/App/StatusCommand.php
@@ -20,7 +20,7 @@ class StatusCommand extends Command {
 		$this->addOption( 'filter', null, InputOption::VALUE_REQUIRED );
 	}
 
-	protected function execute( InputInterface $input, OutputInterface $output ) {
+	protected function execute( InputInterface $input, OutputInterface $output ): int {
 		$project = $input->getArgument( 'project' );
 		$variant = $input->getOption( 'variant' ) ?: $this->defaultVariant;
 		$filter = $input->getOption( 'filter' );
@@ -48,12 +48,14 @@ class StatusCommand extends Command {
 				throw new RuntimeException( "Unknown repo type '$type' for repository: $name" );
 			}
 
-			$process = new Process( $command );
+			$process = Process::fromShellCommandline( $command );
 			$process->setTimeout( 10 );
 			$process->setWorkingDirectory( $base );
 			$processes->attach( $process );
 		}
 
 		$this->runParallelWithOutput( $processes, $output );
+
+		return 0;
 	}
 }

commit 248a1ec7db9e827c3b4989ee285132c4135a27be

repong: Fix backport command
Return was placed too early.

Fixes: 791036174ab9c3baafbcf79cb69c54cc6f800d8a
Change-Id: Idea08126a2fc69f633acd96df740af9ff3899221

repong/src/App/BackportCommand.php
@@ -74,10 +74,10 @@ class BackportCommand extends Command {
 			$process = Process::fromShellCommandline( $command );
 			$process->setTimeout( 900 );
 			$processes->attach( $process );
-
-			return 0;
 		}
 
 		$this->runParallelWithOutput( $processes, $output );
+
+		return 0;
 	}
 }