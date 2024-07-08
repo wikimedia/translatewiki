fb3288734177b80aa5a31dca7c0a12107de6ec8e
Abijeet <abijeetpatro@gmail.com>
2024-07-08 16:00:31 +0530

    GapFinder: Stop loading group configuration
    Bug: T369496
    Change-Id: Icc979298879a3bfb67f940d77fa6c7ebba440522

--- mw-config/TranslateSettings.php
+++ mw-config/TranslateSettings.php
@@ -264,7 +264,6 @@ $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/CommonsAndroid.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/ConvenientDiscussions.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/CopyPatrol.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/DiscordWikiBot.yaml";
-$wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/GapFinder.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/global-search.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/GrantMetrics.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Wikimedia/jquery.uls.yaml";
