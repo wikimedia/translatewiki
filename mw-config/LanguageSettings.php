a2e9735401c556ce6e048d2f0f2b286d3021c19e
Amir E. Aharoni <amir.aharoni@mail.huji.ac.il>
2024-09-03 23:46:40 -0400

    Remove nan-latn-tailo, added to Names.php
    Bug: T369899
    Change-Id: I5a6ee7d1faf4d05d9ea666d6441ea44c639d1600

--- mw-config/LanguageSettings.php
+++ mw-config/LanguageSettings.php
@@ -99,7 +99,6 @@ $wgExtraLanguageNames = [
 	'nan-hani' => '閩南語（漢字）', # Minnan (Han scripts - defaults to Traditional Han) / Winston Sung 2024-03-07
 	'nan-hans' => '闽南语（简化汉字）', # Minnan (Simplified Han script) / Winston Sung 2024-03-07
 	'nan-latn' => 'Bân-lâm-gú (Lô-má-jī)', # Minnan (Latin script, multiple romanizations - defaults to Pe̍h-ōe-jī) / Winston Sung 2024-03-07
-	'nan-latn-tailo' => 'Bân-lâm-gú (Tâi-lô)', # Minnan (Latin script, Tâi-lô) / Winston Sung 2024-03-07
 	'njo' => 'Ao', # Ao Naga
 	'nnh' => 'ngiembɔɔn', # Ngiemboon / Amir 2024-08-07
 	'nod-thai' => 'คำเมือง', # Northern Thai / Siebrand 2017-05-20
