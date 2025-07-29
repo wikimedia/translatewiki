wikimedia/translatewiki

commit 1b9018a69a9f51ca3976e67d01777c89d8423a12

Update int-wikiinfo to sync with tool repo instead of legacy textdomain
To allow for more regular deployments and easier adding/changing
of messages, these are now maintained in the same repo as the tool
instead of as a legacy built-in textdomain.

See also https://gerrit.wikimedia.org/r/c/labs/tools/wikiinfo/+/1173948
which moves these messages from
https://gerrit.wikimedia.org/g/labs/tools/intuition/
to
https://gerrit.wikimedia.org/g/labs/tools/wikiinfo/

See also https://gerrit.wikimedia.org/r/c/labs/tools/intuition/+/1173957
which removes these messages.

I don't remember why "title" was marked as ignored, since it seems
useful to localise. I've marked this optional now.

Change-Id: I0fc281e11b54f401e4e62a4999ccbf65e8c7420b

mw-config/TranslateSettings.php
@@ -420,6 +420,7 @@ $wgTranslateGroupFiles[] = "$GROUPS/Intuition/orphantalk.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Intuition/raun.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Intuition/refill.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Intuition/web.yaml";
+$wgTranslateGroupFiles[] = "$GROUPS/Intuition/wikinfo.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Intuition/commtech-commons.yaml";
 $wgNamespaceAliases['Toolserver'] = 1240;
 $wgNamespaceAliases['Toolserver_talk'] = 1240;

groups/Intuition/wikiinfo.yaml
@@ -1,0 +1,24 @@
+---
+BASIC:
+  id: int-wikiinfo
+  label: Wiki Info
+  namespace: NS_INTUITION
+  class: FileBasedMessageGroup
+
+MANGLER:
+  class: StringMatcher
+  prefix: getwikiapi-
+  patterns:
+    - "*"
+
+FILES:
+  format: Json
+  sourcePattern: "%GROUPROOT%/int-wikiinfo/i18n/%CODE%.json"
+
+TAGS:
+  optional:
+    - getwikiapi-title
+
+VALIDATORS:
+  - id: NumericalParameter
+    insertable: true

repoconfig.yaml
@@ -1193,6 +1193,10 @@ wikimedia-tools:
       type: wmgerrit
       url: https://gerrit.wikimedia.org/r/labs/tools/intuition-web
       url|export: ssh://l10n-bot@gerrit.wikimedia.org:29418/labs/tools/intuition-web
+    int-wikiinfo:
+      type: wmgerrit
+      url: https://gerrit.wikimedia.org/r/labs/tools/wikiinfo
+      url|export: ssh://l10n-bot@gerrit.wikimedia.org:29418/labs/tools/wikiinfo
     int-commtech-commons:
       type: github
       url: https://github.com/wikimedia/CommonsNotifier.git

commit d90593cd546a97b9929bf8902a28a0ba0d0f7f90

Fix typo
Change-Id: I9e2100f7d0725a2a54a7ba2b3d7266f31277310b
Fixes: 1b9018a69a9f51ca3976e67d01777c89d8423a12

mw-config/TranslateSettings.php
@@ -420,7 +420,7 @@ $wgTranslateGroupFiles[] = "$GROUPS/Intuition/orphantalk.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Intuition/raun.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Intuition/refill.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Intuition/web.yaml";
-$wgTranslateGroupFiles[] = "$GROUPS/Intuition/wikinfo.yaml";
+$wgTranslateGroupFiles[] = "$GROUPS/Intuition/wikiinfo.yaml";
 $wgTranslateGroupFiles[] = "$GROUPS/Intuition/commtech-commons.yaml";
 $wgNamespaceAliases['Toolserver'] = 1240;
 $wgNamespaceAliases['Toolserver_talk'] = 1240;