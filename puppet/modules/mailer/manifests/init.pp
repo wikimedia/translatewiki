57b0a0a79e3d19955c7ee2c2367584962c426096
Niklas Laxström <niklas.laxstrom@gmail.com>
2025-07-30 13:53:45 +0300

    Add DNSBL filtering on email
    To reduce the amount of incoming spam.
    
    https://docs.spamhaus.com/datasets/docs/source/40-real-world-usage/PublicMirrors/MTAs/020-Postfix.html
    
    Also update the puppet-postfix module.
    
    Change-Id: I12b4873c97972547ab0424e321e2c9af7e449993

--- puppet/modules/mailer/manifests/init.pp
+++ puppet/modules/mailer/manifests/init.pp
@@ -21,31 +21,42 @@ class mailer (
   }
 
   postfix::config {
-    'mydomain':                    value => $domain;
-    'mydestination':               value => "${domain}, localhost";
-    'relay_domains':               value => $domain;
-    'virtual_alias_maps':          value => 'regexp:/etc/postfix/virtual_regexp';
-    'header_size_limit':           value => '4096000';
-    'smtpd_helo_required':         value => 'yes';
-    'smtpd_helo_restrictions':     value => @(HEREDOC/L)
-       permit_inet_interfaces permit_sasl_authenticated \
-       reject_invalid_helo_hostname reject_non_fqdn_helo_hostname \
-       reject_unknown_helo_hostname
-       |- HEREDOC
+    'mydomain':                     value => $domain;
+    'mydestination':                value => "${domain}, localhost";
+    'relay_domains':                value => $domain;
+    'virtual_alias_maps':           value => 'regexp:/etc/postfix/virtual_regexp';
+    'header_size_limit':            value => '4096000';
+    'smtpd_helo_required':          value => 'yes';
+    'smtpd_helo_restrictions':      value => @(HEREDOC/L)
+      permit_inet_interfaces permit_sasl_authenticated \
+      reject_invalid_helo_hostname reject_non_fqdn_helo_hostname \
+      reject_unknown_helo_hostname
+      |- HEREDOC
     ;
-    'smtpd_sender_restrictions':   value => @(HEREDOC/L)
-       permit_inet_interfaces permit_sasl_authenticated \
-       reject_unknown_reverse_client_hostname reject_unknown_client_hostname \
-       reject_unknown_sender_domain
-       |- HEREDOC
+    'smtpd_sender_restrictions':    value => @(HEREDOC/L)
+      permit_inet_interfaces permit_sasl_authenticated \
+      reject_unknown_reverse_client_hostname reject_unknown_client_hostname \
+      reject_unknown_sender_domain
+      |- HEREDOC
     ;
-    'smtpd_relay_restrictions':    value => 'permit_mynetworks permit_sasl_authenticated defer_unauth_destination';
-    'sender_canonical_maps':       value => 'tcp:localhost:10001';
-    'sender_canonical_classes':    value => 'envelope_sender';
-    'recipient_canonical_maps':    value => 'tcp:localhost:10002';
-    'recipient_canonical_classes': value => 'envelope_recipient,header_recipient';
-    'smtpd_milters':               value => 'inet:localhost:8891';
-    'non_smtpd_milters':           value => '$smtpd_milters';
+    'smtpd_recipient_restrictions': value => @(HEREDOC/L)
+      permit_mynetworks, \
+      permit_sasl_authenticated, \
+      reject_unauth_destination, \
+      reject_rbl_client                zen.spamhaus.org=127.0.0.[2..11], \
+      reject_rhsbl_sender              dbl.spamhaus.org=127.0.1.[2..99], \
+      reject_rhsbl_helo                dbl.spamhaus.org=127.0.1.[2..99], \
+      reject_rhsbl_reverse_client      dbl.spamhaus.org=127.0.1.[2..99], \
+      warn_if_reject reject_rbl_client zen.spamhaus.org=127.255.255.[1..255]
+      |- HEREDOC
+    ;
+    'smtpd_relay_restrictions':     value => 'permit_mynetworks permit_sasl_authenticated reject_unauth_destination';
+    'sender_canonical_maps':        value => 'tcp:localhost:10001';
+    'sender_canonical_classes':     value => 'envelope_sender';
+    'recipient_canonical_maps':     value => 'tcp:localhost:10002';
+    'recipient_canonical_classes':  value => 'envelope_recipient,header_recipient';
+    'smtpd_milters':                value => 'inet:localhost:8891';
+    'non_smtpd_milters':            value => '$smtpd_milters';
   }
 
   postfix::hash { '/etc/postfix/sasl_passwd':
